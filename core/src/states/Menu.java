b10d04b4963f6360086bab63d179bd4c58250032
Darius <darius@tryskubai.lt>
2018-02-10 23:40:17 +0200

    menu v1

--- core/src/states/Menu.java
+++ core/src/states/Menu.java
@@ -6,10 +6,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Game;
@@ -23,10 +26,12 @@ public class Menu extends GameState {
 	Viewport viewport;
 	TextButton button_play;
 	TextButton button_exit;
+	GameStateManager gsm;
 
-	public Menu(GameStateManager gsm) {
+	public Menu(final GameStateManager gsm) {
 		super(gsm);
 
+		this.gsm = gsm;
 		Skin skin = new Skin(Gdx.files.internal("skins2/flat-earth-ui.json"));
 		viewport = new FitViewport(Game.V_WIDTH, Game.V_HEIGHT, hudcam);
 		stage = new Stage(viewport, sb);
@@ -34,6 +39,12 @@ public class Menu extends GameState {
 
 		button_play = new TextButton("PLAY", skin);
 		button_play.setSize(400, 40);
+		button_play.addCaptureListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				gsm.setState(new Play(gsm));
+			}
+		});
 
 		button_exit = new TextButton("EXIT", skin);
 		button_exit.setSize(400, 40);
@@ -42,7 +53,7 @@ public class Menu extends GameState {
 		table.row().pad(10);
 		table.add(button_exit);
 
-		table.setPosition((Game.V_WIDTH / 2) - (table.getWidth() / 2), 200);
+		table.setPosition((Game.V_WIDTH / 2) - (table.getWidth() / 2), (Game.V_HEIGHT / 2) - (table.getHeight() / 2));
 		
 		Gdx.input.setInputProcessor(stage);
 		stage.addActor(table);
